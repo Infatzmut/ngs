c323bc196063994091beb46d6b745fba105a5b26
Infatzmut <fabrizzio1996@gmail.com>
2019-08-31 11:01:47 -0500

    requerimiento cumplido

--- 1-getting-started/2-timers/7-no-guarantees.js
+++ 1-getting-started/2-timers/7-no-guarantees.js
@@ -3,14 +3,17 @@
   500,
 );*/
 
-const test = setTimeout(
+/*const test = setTimeout(
   () => console.log('Hello after 0.5 seconds. MAYBE!'),
   500,
-);
+);*/
+const abc = ()=>setTimeout(()=>imprime(),500)
+
 const imprime = ()=>console.log("Hello after 0.5 seconds. Maybe!")
 //const test1 =setTimeout(()=>imprime(),500)
 for (let i = 0; i < 10; i++) {
   // Block Node Synchronously
-  setTimeout(()=>imprime(),500)
+  //setTimeout(()=>imprime(),500)
+  abc()
 
 }
