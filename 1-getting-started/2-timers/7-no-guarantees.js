9404c1a989792425e84c7f3937814df6941d3d6e
Infatzmut <fabrizzio1996@gmail.com>
2019-08-31 10:56:22 -0500

    requerimiento funcionando fuera de variable

--- 1-getting-started/2-timers/7-no-guarantees.js
+++ 1-getting-started/2-timers/7-no-guarantees.js
@@ -1,8 +1,16 @@
-setTimeout(
+/*setTimeout(
+  () => console.log('Hello after 0.5 seconds. MAYBE!'),
+  500,
+);*/
+
+const test = setTimeout(
   () => console.log('Hello after 0.5 seconds. MAYBE!'),
   500,
 );
-
-for (let i = 0; i < 1e10; i++) {
+const imprime = ()=>console.log("Hello after 0.5 seconds. Maybe!")
+//const test1 =setTimeout(()=>imprime(),500)
+for (let i = 0; i < 10; i++) {
   // Block Node Synchronously
+  setTimeout(()=>imprime(),500)
+
 }
