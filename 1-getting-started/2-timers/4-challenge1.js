361c8982b2488d2f308a4efeb2903630b94f9e74
Infatzmut <fabrizzio1996@gmail.com>
2019-08-31 07:42:17 -0500

    Solved challenge timer-challenge 01

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,12 @@
-const theOneFunc = () => {};
+const theOneFunc = (x,y) => {
+    timer(x)
+    timer(y)
+};
 
-setTimeout(theOneFunc, 4 * 1000);
-
+const timer = (a) =>setTimeout(()=>console.log(`Hello after ${a} seconds`),a*1000)
 // Hello after 4 seconds
 
 // Hello after 8 seconds
 
 // You can define only ONE function
+theOneFunc(4,8)
